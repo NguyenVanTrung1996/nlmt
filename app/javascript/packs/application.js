7b8f005ffbf5d24aa192793b26d2ad5d55ac4fd9
NguyenVanTrung1996 <nguyenvantrung2015@gmail.com>
2021-04-09 21:34:45 +0900

    add vue project init

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,19 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import Vue from "vue";
+import App from "../app.vue";
+// import router from "./router";
+// import store from "./store";
+import vuetify from "./plugins/vuetify";
+// import i18n from "./plugins/i18n";
+// import moment from "moment";
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+Vue.config.productionTip = false;
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+new Vue({
+    el: "#app",
+    // i18n,
+    vuetify,
+    // router,
+    // store,
+    // moment,
+    render: h => h(App)
+}).$mount("#app");
